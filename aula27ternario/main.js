1bbb645bdbbeb2e6a242e80a6d1b0d5b317c0d1e
MarllonAnisio <marllon.anisio@hotmail.com>
2026-01-21 12:57:16 -0300

    aula sobre ternario no Js

---
+++ aula27ternario/main.js
@@ -0,0 +1,11 @@
+const notaDaProva = 6.5;
+
+/**
+ * Usando o operador ternário para determinar se o aluno está aprovado ou reprovado
+ * voce elimina o bloco if e else do programa e deixa o codigo limpo e objetivo, 
+ * se vor verdadeira a condição vai retornar a primeira expressão, se for falsa retorna a segunda expressão
+ */
+const resultado = notaDaProva >= 7 ? "Aprovado" : "Reprovado";
+
+console.log(`O aluno está: ${resultado}`);
+
