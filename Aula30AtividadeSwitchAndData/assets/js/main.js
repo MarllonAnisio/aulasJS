02f262ebd0fe70715f5d218aea6b0ef64381f25d
MarllonAnisio <marllon.anisio@hotmail.com>
2026-01-23 16:49:10 -0300

    maneira pratica com artificios ja criados do JS

--- Aula30AtividadeSwitchAndData/assets/js/main.js
+++ Aula30AtividadeSwitchAndData/assets/js/main.js
@@ -1,3 +1,15 @@
 const h1 = document.querySelector('.conteiner h1');
 
 const data = new Date();
+/**
+ * Opções de formatação para data e hora
+ * @type {Object}
+ * usando Intl.DateTimeFormatOptions
+ *  estudar lendo: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
+ */
+const opcoes = {
+   dateStyle: 'full',
+   timeStyle: 'short'
+};
+
+h1.innerHTML = data.toLocaleString('pt-BR', opcoes);
