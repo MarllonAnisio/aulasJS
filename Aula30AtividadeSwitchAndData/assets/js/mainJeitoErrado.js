e7e25397d57a2d7a81f390ed6cbbab9056fb23c2
MarllonAnisio <marllon.anisio@hotmail.com>
2026-01-23 16:57:04 -0300

    refatoração da maneira 'errada' para uma mais viavel utilizando arrays para armazenar os valores

--- Aula30AtividadeSwitchAndData/assets/js/mainJeitoErrado.js
+++ Aula30AtividadeSwitchAndData/assets/js/mainJeitoErrado.js
@@ -3,78 +3,19 @@ const h1 = document.querySelector('.conteiner h1');
 const data = new Date();
 
 function getNomeMes(numeroMes){
-    let nomeMes;
-    switch (numeroMes) {
-        case 0:
-            nomeMes = "Janeiro";
-            break;
-        case 1:
-            nomeMes = "Fevereiro";
-            break;
-        case 2:
-            nomeMes = "Março";
-            break;
-        case 3:
-            nomeMes = "Abril";
-            break;
-        case 4:
-            nomeMes = "Maio";
-            break;
-        case 5:
-            nomeMes = "Junho";
-            break;
-
-        case 6:
-            nomeMes = "Julho";
-            break;
-        case 7:
-            nomeMes = "Agosto";
-            break;
-        case 8:
-            nomeMes = "Setembro";
-            break;
-        case 9:
-            nomeMes = "Outubro";
-            break;
-        case 10:
-            nomeMes = "Novembro";
-            break;  
-        case 11:
-            nomeMes = "Dezembro";
-            break;
-        default:
-            nomeMes = "Mês inválido";
-    }
-    return nomeMes;
+    const meses = [
+        "Janeiro","Fevereiro","Março","Abril","Maio","Junho","Junho",
+        "Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"
+    ];
+    
+    return meses[numeroMes];
 }
 function retornaDiaSemana(numeroDia) {
-    let diaSemana;
-    switch (numeroDia) {
-        case 1:
-            diaSemana = "Domingo";
-            break;
-        case 2:
-            diaSemana = "Segunda-feira";
-            break;
-        case 3:
-            diaSemana = "Terça-feira";
-            break;
-        case 4:
-            diaSemana = "Quarta-feira";
-            break;
-        case 5:
-            diaSemana = "Quinta-feira";
-            break;
-        case 6:
-            diaSemana = "Sexta-feira";
-            break;
-        case 7:
-            diaSemana = "Sábado";
-            break;
-        default:
-            diaSemana = "Número inválido";
-    }
-    return diaSemana;
+    const diasSemana = [
+        "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira",
+        "Quinta-feira", "Sexta-feira", "Sábado"
+    ];
+    return diasSemana[numeroDia];
 }
 
 function zeroAEsquerda(num){
@@ -84,7 +25,7 @@ function zeroAEsquerda(num){
 function criaData(data){
     const diaSemana = data.getDay();
     const numeroMesMes = data.getMonth();
-
+    
     const NomeMes = getNomeMes(numeroMesMes);
     const nomeDia = retornaDiaSemana(diaSemana);
 
