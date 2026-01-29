21089ef1080b2b00949e8a39f3d8cff8a947a6d4
MarllonAnisio <marllon.anisio@hotmail.com>
2026-01-29 16:24:40 -0300

    atividade sobre altura largura

---
+++ aula43ExercicioLogica2/main.js
@@ -0,0 +1,20 @@
+/**
+ * Função que verifica se a resolução é paisagem retornando true ou false
+ */
+
+function ePaisagem(autura, largura) {
+    if (largura === autura)
+         return 'A imagem é quadrada';
+    return largura > autura;
+};
+
+console.log(ePaisagem(1080, 1920));
+console.log(ePaisagem(1920, 1080));
+console.log(ePaisagem(1080, 1080));
+
+// com arrow function
+const ePaisagemArrow = (altura, largura) => largura === altura ? "é um quadrado" : largura > altura;
+
+console.log(ePaisagemArrow(1080, 1920));
+console.log(ePaisagemArrow(1920, 1080));
+console.log(ePaisagemArrow(1080, 1080));
