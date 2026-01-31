44a8797f295be429c6ae8f30094739d11cb01a7a
MarllonAnisio <marllon.anisio@hotmail.com>
2026-01-30 23:06:08 -0300

    feat: isolando armazenador de tarefas

---
+++ aula48ListaTarefasCrud/assets/js/modules/storage.js
@@ -0,0 +1,22 @@
+
+/**
+ * 
+ * @param {*} ulTarefas  Que é uma lista de Tarefas criadas pelo usuario 
+ * @constant liTarefas está recebendo atraves do querySelector todos (li) que estáo no Ul
+ */
+export function salvarTarefas(ulTarefas) {
+    const liTarefas = ulTarefas.querySelectorAll('li');
+    //lista para armazenar as tarefas
+    const listaDeTarefas = [];
+    
+    for (let tarefa of liTarefas) {
+        const span = tarefa.querySelector('.texto-tarefa');
+        
+        if (span) {
+            listaDeTarefas.push(span.innerText);
+        }
+    }
+    
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+}
