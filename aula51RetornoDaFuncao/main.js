a512608d844588fb7ecdcc13d9672f5948fcd02c
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-05 21:36:03 -0300

    retornando uma função para uma variavel que vira uma função!

--- aula51RetornoDaFuncao/main.js
+++ aula51RetornoDaFuncao/main.js
@@ -40,6 +40,21 @@ const pizza = {
     massa: null
 };
 
-
+//fabricaPizza("peperoni", pizza);
 const recebe = fabricaPizza("portuguesa");
 console.log(recebe.tipo + ": " + recebe.recheio);
+
+
+function criaMultiplicador(multiplicador){
+    return function(n){
+       return n * multiplicador;
+    }
+}
+
+let duplicar = criaMultiplicador(2);
+let triplica = criaMultiplicador(3);
+let quadruplica = criaMultiplicador(4);
+
+console.log(duplicar(30));
+console.log(triplica(45));
+console.log(quadruplica(8));
