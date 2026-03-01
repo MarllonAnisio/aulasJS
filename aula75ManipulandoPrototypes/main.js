c51ddb85d6e6b82876388c72fd7ac1754ec8c202
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-28 21:35:02 -0300

    aprendendo prototypes

--- aula75ManipulandoPrototypes/main.js
+++ aula75ManipulandoPrototypes/main.js
@@ -5,18 +5,28 @@
  * é uma especie de herança que evita que sempre apareca para o usuario
  */
 
+
 function Objeto(nome, preco){
-    this.nome = nome
-    this.preco = preco
+    this.nome = nome;
+    this.preco = preco;
 }
 
-Objeto.prototype.aumento = () => preco *= 1.10;
-Objeto.prototype.desconto = () => preco *=  0.90;
+Objeto.prototype.aumento = function(aumento) {
+    const valorAumento = this.preco * (aumento / 100)
+    this.preco += valorAumento;
+    console.log(`O aumento ${aumento} e o valor passou a ser: ${this.preco.toFixed(2)}`);
+};
+Objeto.prototype.desconto = function(desconto) {
+    const valorDesconto = this.preco * (desconto / 100)
+    this.preco -= valorDesconto;
+    console.log(`O desconto ${desconto} e o valor passou a ser: ${this.preco.toFixed(2)}`);
+};
 
-const lapis = new Objeto("lapis", 100);
-const caneta = new Objeto("caneta", 49);
+const lapis = new Objeto("lapis", 1000);
+const caneta = new Objeto("caneta", 50);
 
-console.log(lapis.aumento());
-console.log(lapis.desconto());
-console.log(caneta.aumento());
-console.log(caneta.desconto());
+
+lapis.aumento(25);
+lapis.desconto(40)
+caneta.aumento(33)
+caneta.desconto(5)
