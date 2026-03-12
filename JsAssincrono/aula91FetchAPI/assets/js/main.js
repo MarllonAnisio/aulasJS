bf4df551a452680e4d9cc1ed08eea18ad74269c8
MarllonAnisio <marllon.anisio@hotmail.com>
2026-03-11 21:31:39 -0300

    refazendo pacotes perdidos

---
+++ JsAssincrono/aula91FetchAPI/assets/js/main.js
@@ -0,0 +1,30 @@
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+async function carregaPagina(el) {
+
+    try{
+     
+        const href = el.getAttribute('href');
+        const response = await fetch(href);
+
+        if(response.status < 200 || response.status > 200) throw new Error('valeime bixão');
+        
+        const html = await response.text();
+        carregaResultado(html);
+    
+    }catch(err){
+        console.log(e)
+    }
+
+}
+function carregaResultado(response) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = response;
+}
